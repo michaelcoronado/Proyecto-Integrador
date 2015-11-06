aa7d1cc0e7ca512c88b6e5a29f6962064e2342d4
esteban513 <esteban513@outlook.com>
2015-11-06 11:09:03 -0500

    botones
    boton de sensibilidad

---
+++ Assets/Grupo 3/sensibilidad.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sensibilidad : MonoBehaviour {
+
+	// Use this for initialization
+	public void sensy (string CambiarEscena)  {
+
+		Application.LoadLevel (CambiarEscena);
+	
+	}
+}
