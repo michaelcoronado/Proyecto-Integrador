dc42918d5f4c988d78bba96bbf3346a4b4dc6e64
jzamora5 <kINGDOMHEARTS9>
2015-11-07 18:18:48 -0500

    Frequency Response Button
    Button for frequency response

---
+++ Assets/Grupo 2/SceneChange.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChange : MonoBehaviour {
+	
+	// Use this for initialization
+	public void scene (string CambiarEscena)  {
+		
+		Application.LoadLevel (CambiarEscena);
+		
+	}
+}
+
