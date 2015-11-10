793d49e246ed202e0281e8f13a6059439df49b77
esteban513 <esteban513@outlook.com>
2015-11-10 16:20:58 -0500

    escena M88 completa
    botones de patrones polares y respuesta en frecuencia

---
+++ Assets/Grupo 3/M88/SceneChange.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CambiarEscena : MonoBehaviour {
+	
+	// Use this for initialization
+	public void scene (string CambiarEscena)  {
+		
+		Application.LoadLevel (CambiarEscena);
+		
+	}
+}
+
